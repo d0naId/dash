d8b0df12a37c0942c835bc84700c211c477b941b
yush <rep.yurii@gmail.com>
2018-11-18 15:19:11 +0300

    add run Dash

--- dinamicDash/runDash.py
+++ dinamicDash/runDash.py
@@ -8,6 +8,29 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
 
+app = dash.Dash(__name__, sharing=True)
+server = app.server
 
 USERNAME_PASSWORD_PAIRS = [
     ['ISD', 'ShowMeTheDash'],['D0naid', 'yoyoyo'],['Stepler', 'yoyoyo']]
+
+app.layout = html.Div(
+    [ # Самый большой контейнер
+    html.Div([html.H1('ISD super dashboard')]), # Заголовок
+    html.Div(
+        [
+        dcc.Checklist(
+                id = 'soc_x',
+                options=[
+                    {'label': 'Пол', 'value': 'SEX'},
+                    {'label': 'Возраст', 'value': 'AGE'}
+                    ],
+                values=['SEX', 'AGE'],
+                labelStyle={'display': 'inline-block'}
+            )
+        ])
+    ]) # Закончился Самый большой контейнер
+
+
+if __name__ == '__main__':
+    app.run_server()

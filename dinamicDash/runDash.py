03cce719616d5603df8b0fe9e0ee0856667cf92b
yush <rep.yurii@gmail.com>
2018-11-19 03:29:16 +0300

    add new core elements

--- dinamicDash/runDash.py
+++ dinamicDash/runDash.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import dash
-import dash_auth
+# import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
 import sqlite3 as sq
@@ -20,15 +20,55 @@ app.layout = html.Div(
     html.Div(
         [
         dcc.Checklist(
-                id = 'soc_x',
+                id = 'EVENT',
                 options=[
-                    {'label': 'Пол', 'value': 'SEX'},
-                    {'label': 'Возраст', 'value': 'AGE'}
+                    {'label': 'доступ разрешен', 'value': 'доступ разрешен'},
+                    {'label': 'был проход (вход)', 'value': 'был проход (вход)'},
+                    {'label': 'был проход (выход)', 'value': 'был проход (выход)'},
+                    {'label': 'ошибочная транзакция', 'value': 'ошибочная транзакция'},
+                    {'label': 'вход после таймаута', 'value': 'вход после таймаута'}
                     ],
-                values=['SEX', 'AGE'],
-                labelStyle={'display': 'inline-block'}
-            )
-        ])
+                values=['доступ разрешен', 'был проход (вход)',
+                'ошибочная транзакция', 'был проход (выход)',
+                'открытие из кассы', 'вход после таймаута'],
+                labelStyle={'display': 'inline-block', 'float':'left'}
+            ),  # Закончился Checklist EVENT
+        dcc.Dropdown(
+            id = 'GROUPER',
+            options=[
+                {'label': '30 мин', 'value': 'HALFH'},
+                {'label': '1 день', 'value': 'DAY'},
+                {'label': '1 неделя', 'value': 'WEEK'},
+                {'label': '1 месяц', 'value': 'MONTH'}
+            ],
+            value='HALFH'
+            ), # Закончился Dropdown
+        dcc.Checklist(
+                id = 'DEVICE',
+                options=[
+                    {'label': 'Тур.№2 KABA-КАССА (2-ой слева)', 'value': 'Тур.№2 KABA-КАССА (2-ой слева)'},
+                    {'label': 'Тур.№3 KABA-ВХОД', 'value': 'Тур.№3 KABA-ВХОД'},
+                    {'label': 'Тур.№1 KABA-ВЫХОД (Левый)', 'value': 'Тур.№1 KABA-ВЫХОД (Левый)'},
+                    {'label': 'Тур.№7 Skidata (Соляная)', 'value': 'Тур.№7 Skidata (Соляная)'},
+                    {'label': 'Тур.№4 KABA-КАССА (Правый)', 'value': 'Тур.№4 KABA-КАССА (Правый)'}
+                    ],
+                values=['Тур.№2 KABA-КАССА (2-ой слева)', 'Тур.№3 KABA-ВХОД',
+                        'Тур.№1 KABA-ВЫХОД (Левый)', 'Тур.№7 Skidata (Соляная)',
+                        'Тур.№4 KABA-КАССА (Правый)'],
+                labelStyle={'display': 'inline-block', 'float':'left'}
+            ),   # Закончился Checklist DEVICE
+        dcc.Input(
+            id = 'HIST',
+            placeholder='Количество дней',
+            type='text',
+            value='3'
+            )  #Закончился Input
+        ], style={'width': '200px',
+                  'float':'left','display': 'inline-block',
+                  'padding-top': '50px'}),  # Dash Core Components
+    # html.Div([
+    #     dcc.Graph(id='bar_time',config={'displayModeBar':False}
+    #     ])
     ]) # Закончился Самый большой контейнер
 
 
--- theor.py
+++ theor.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import dash
-import dash_auth
+# import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
 import sqlite3 as sq

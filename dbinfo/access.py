2d06c5f76cca1548de5a5d7daebcbda8294b1ead
astepankoff <astepankoff@mail.ru>
2018-12-08 15:19:06 +0300

    add app1.py

---
+++ dbinfo/access.py
@@ -0,0 +1,10 @@
+db_acc_dict = {
+'host': '178.209.113.34',
+'port': 1521,
+'service_name': 'chudo',
+'user': 'kurort',
+'passwd': 'kurort'}
+
+USERNAME_PASSWORD_PAIRS = [['ISD', 'ShowMeTheDash'],
+                           ['D0naid', 'yoyoyo'],
+                           ['Stepler', 'yoyoyo']]
